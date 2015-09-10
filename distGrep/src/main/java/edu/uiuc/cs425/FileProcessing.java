eef6d9e84752619e7ce55365613c70a62b357188
Banu Muthukumar <banu27@gmail.com>
2015-09-09 19:27:00 -0500

    attempt16 sending output

--- distGrep/src/main/java/edu/uiuc/cs425/FileProcessing.java
+++ distGrep/src/main/java/edu/uiuc/cs425/FileProcessing.java
@@ -37,8 +37,8 @@ public class FileProcessing {
 			String sshCommand = "ssh";
 			String command = "grep";
 			//sshCommand , m_sVMName , 
-			Commons.SystemCommand(new String [] { "ls"," > newfile.txt"});
-			//Commons.SystemCommand(new String[] { command ,"as",username+"/logs/log1.txt"});
+			//Commons.SystemCommand(new String [] { "ls"," > newfile.txt"});
+			Commons.SystemCommand(new String[] { "bash","-c",command +" as "+username+"/logs/log1.txt"});
 			//> "+username+"/logs/grepResult.out & " });			
 			System.out.println("grep call over"); //Why is this showing up on my command line??
 		}
