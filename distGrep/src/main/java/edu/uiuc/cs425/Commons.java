e267ca5def79a0e92a893d0edb078e89b8a5f49a
Banu Muthukumar <banu27@gmail.com>
2015-09-11 10:17:18 -0500

    Controller 2

--- distGrep/src/main/java/edu/uiuc/cs425/Commons.java
+++ distGrep/src/main/java/edu/uiuc/cs425/Commons.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public final class Commons {
 	public static final int SUCCESS = 0;
 	public static final int FAILURE = 1;
-	public static final int SERVICE_PORT = 9090;
+	public static final int SERVICE_PORT = 9091;
 	public static final int NETWORK_THREAD_COUNT = 4;
 	public static final int WORKER_THREAD_COUNT = 8;
 	public static final int MASTER = 0;
